fe6f1a64dc67730de2808c84c680248aafbb9551
Zé Marcos <contato.marcosrf@gmail.com>
2023-05-15 21:40:42 -0300

    wip

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -5,44 +5,18 @@
         </h2>
     </x-slot>
 
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+    <x-container>
 
-            <form action="{{ route('question.store') }}" method="POST">
-                @csrf
+        <x-form action="{{ route('question.store') }}" method="POST">
 
-                <div class="mb-4">
-                    <label for="question" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
-                        Question
-                    </label>
+            <x-textarea name="question" label="Question" />
 
-                    <textarea id="question" name="question" rows="4"
-                        class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                        placeholder="Ask me anything...">{{ old('question') }}</textarea>
-                    @error('question')
-                        <div class="text-red-500 mt-2 text-sm">
-                            {{ $message }}
-                        </div>
-                    @enderror
-                </div>
+            <x-primary-button type="submit"> Ask </x-primary-button>
 
-                <button type="submit"
-                    class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5
-                     mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">
-                    Save
-                </button>
+            <x-secondary-button type="reset"> Cancel </x-secondary-button>
 
-                <button type="reset"
-                    class="py-2.5 px-5 mr-2 mb-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4
-                    focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600
-                    dark:hover:text-white dark:hover:bg-gray-700">
-                    Cancel
-                </button>
+        </x-form>
 
+    </x-container>
 
-
-            </form>
-
-        </div>
-    </div>
 </x-app-layout>
