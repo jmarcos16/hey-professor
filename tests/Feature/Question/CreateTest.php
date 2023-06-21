82312381a592c2d8362c740a7604aa33c82ae504
Zé Marcos <contato.marcosrf@gmail.com>
2023-06-21 20:42:42 -0300

    hey-17: it should make sure that only authenticated users can create a question

--- tests/Feature/Question/CreateTest.php
+++ tests/Feature/Question/CreateTest.php
@@ -4,12 +4,10 @@ use App\Models\User;
 
 use function Pest\Laravel\{actingAs, assertDatabaseCount, assertDatabaseHas, post};
 
-beforeEach(function () {
-    $this->user = User::factory()->create();
-    actingAs($this->user);
-});
-
 it('should be able to create a new question bigger than 255 characters', function () {
+    $user = User::factory()->create();
+    actingAs($user);
+
     $request = post(route('question.store'), [
         'question' => str_repeat('*', 260) . '?',
     ]);
@@ -20,6 +18,9 @@ it('should be able to create a new question bigger than 255 characters', functio
 });
 
 it('should checke if ends with question mark ?', function () {
+    $user = User::factory()->create();
+    actingAs($user);
+
     $request = post(route('question.store'), [
         'question' => str_repeat('*', 10),
     ]);
@@ -29,6 +30,9 @@ it('should checke if ends with question mark ?', function () {
 });
 
 it('should have at last 10 characters', function () {
+    $user = User::factory()->create();
+    actingAs($user);
+
     $request = post(route('question.store'), [
         'question' => str_repeat('*', 8) . '?',
     ]);
@@ -41,6 +45,9 @@ it('should create as a draft all the time', function () {
     $user = User::factory()->create();
     actingAs($user);
 
+    $user = User::factory()->create();
+    actingAs($user);
+
     post(route('question.store'), [
         'question' => str_repeat('*', 260) . '?',
     ]);
@@ -49,4 +56,10 @@ it('should create as a draft all the time', function () {
         'question' => str_repeat('*', 260) . '?',
         'draft'    => true,
     ]);
+});
+
+it('should make sure that only authenticated users can create a question', function () {
+    post(route('question.store'), [
+        'question' => str_repeat('*', 260) . '?',
+    ])->assertRedirect(route('login'));
 });
